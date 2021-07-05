b5e8e0b7d832ee39e8f67ce93a77da57d6adfc77
Barsik_Kato <noran-kotov@yandex.ru>
2021-07-05 11:49:19 +0300

    Доделаны модульные тесты

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -12,16 +12,44 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1() //Тест поиска начальной точки
         {
-            CriticalPath cp = new CriticalPath(@"Данные задачи.csv", null);
+            CriticalPath cp = new CriticalPath(@"E:\vsprojects\KotovExam\KotovExam\Данные задачи.csv", null);
             cp.ReadData();
             Assert.AreEqual("1", cp.FindStartingPos());
         }
         [TestMethod]
         public void TestMethod2() //Тест поиска конечной точки
         {
-            CriticalPath cp = new CriticalPath(@"Данные задачи.csv", null);
+            CriticalPath cp = new CriticalPath(@"E:\vsprojects\KotovExam\KotovExam\Данные задачи.csv", null);
             cp.ReadData();
             Assert.AreEqual("7", cp.FindEndingPos());
+        }
+        [TestMethod]
+        public void TestMethod3() //Тест поиска длины критического пути
+        {
+            CriticalPath cp = new CriticalPath(@"E:\vsprojects\KotovExam\KotovExam\Данные задачи.csv", null);
+            cp.ReadData();
+            cp.CalculatePathes();
+            var criticalPath = cp.FindCriticalPath();
+            Assert.AreEqual(29, criticalPath[0].length);
+        }
+        [TestMethod]
+        public void TestMethod4() //Тест последней точки
+        {
+            CriticalPath cp = new CriticalPath(@"E:\vsprojects\KotovExam\KotovExam\Данные задачи.csv", null);
+            cp.ReadData();
+            cp.CalculatePathes();
+            var criticalPath = cp.FindCriticalPath();
+            Assert.AreEqual(cp.FindEndingPos(), criticalPath[0].lastPoint);
+        }
+        [TestMethod]
+        public void TestMethod5() //Тест сохранения
+        {
+            CriticalPath cp = new CriticalPath(@"E:\vsprojects\KotovExam\KotovExam\Данные задачи.csv", "TestResult.csv");
+            cp.ReadData();
+            cp.CalculatePathes();
+            var criticalPath = cp.FindCriticalPath();
+            cp.WriteToFile(criticalPath);
+            Assert.IsTrue(File.Exists("TestResult.csv"));
         }
     }
 }
